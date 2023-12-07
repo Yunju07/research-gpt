15ba695a1c3bbad3276e0cd23a6c0537c2ed8c07
yunju <2020017065@hanyang.ac.kr>
2023-12-07 09:50:53 +0900

    수정

--- practice/pdfExtractTest.py
+++ practice/pdfExtractTest.py
@@ -3,7 +3,7 @@ from PyPDF2 import PdfReader
 import tabula
 
 # 실습 pdf 파일 경로
-PDF_FILE_PATH = "./pdf/chart_test.pdf"
+PDF_FILE_PATH = "./pdf/2021 DB 프로젝트1.pdf"
 
 # PyMuPDF 
 # doc = fitz.open(PDF_FILE_PATH)
@@ -12,13 +12,13 @@ PDF_FILE_PATH = "./pdf/chart_test.pdf"
 #     print(text)
 
 #  PyPDF2 
-# reader = PdfReader(PDF_FILE_PATH)
-# pages = reader.pages
-# text = ""
-# for page in pages:
-#   sub = page.extract_text()
-#   text += sub
-# print(text)
+reader = PdfReader(PDF_FILE_PATH)
+pages = reader.pages
+text = ""
+for page in pages:
+  sub = page.extract_text()
+  text += sub
+print(text)
 
 # tabula
 dfs = tabula.read_pdf(PDF_FILE_PATH, pages="all", encoding='CP949')
